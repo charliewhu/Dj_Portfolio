80b5ad75bf1bb4a461a7324389fd12d3179dba7b
charl <charl@DESKTOP-A4DK2O9>
2021-11-26 14:45:31 +0000

    changed image field in Project model

--- web_project/settings.py
+++ web_project/settings.py
@@ -15,7 +15,7 @@ import os
 import dotenv
 import dj_database_url
 import django_heroku
-import storages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
