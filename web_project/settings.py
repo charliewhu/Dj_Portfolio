f071c44183c077a15b10bf39a4e7a9f29e7cf7d4
charl <charl@DESKTOP-A4DK2O9>
2021-11-26 16:15:32 +0000

    s3 config

--- web_project/settings.py
+++ web_project/settings.py
@@ -139,15 +139,16 @@ AWS_STORAGE_BUCKET_NAME = 'charlieavery'
 
 AWS_DEFAULT_ACL = None
 AWS_S3_FILE_OVERWRITE = False
-#AWS_S3_REGION_NAME = "eu-west-2"
+AWS_S3_REGION_NAME = "eu-west-2"
 AWS_S3_ADDRESSING_STYLE = "virtual"
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL =  f'https://{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-2.amazonaws.com/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
     ]
