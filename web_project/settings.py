a7e57aaa716230270bd8cc538f2c44523547abfc
charl <charl@DESKTOP-A4DK2O9>
2021-11-26 11:40:29 +0000

    changed image field in Project model

--- web_project/settings.py
+++ web_project/settings.py
@@ -34,6 +34,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
+
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'charlieavery.herokuapp.com'
