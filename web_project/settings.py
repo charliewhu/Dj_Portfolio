d017c12459bce213d8ab954c05099688f08a8147
charl <charl@DESKTOP-A4DK2O9>
2021-11-26 19:23:47 +0000

    s3 config

--- web_project/settings.py
+++ web_project/settings.py
@@ -146,7 +146,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3..{AWS_S3_REGION_NAME}.amazonaws.com'
+STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
