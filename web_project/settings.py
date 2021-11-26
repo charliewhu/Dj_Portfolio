8d66708320bb91959e150554dce8be34487e4d46
charl <charl@DESKTOP-A4DK2O9>
2021-11-26 14:39:32 +0000

    changed image field in Project model

--- web_project/settings.py
+++ web_project/settings.py
@@ -152,8 +152,8 @@ AWS_S3_ADDRESSING_STYLE = "virtual"
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = os.environ.get('DEFAULT_FILE_STORAGE')
+STATICFILES_STORAGE = os.environ.get('STATICFILES_STORAGE')
 
 
 
